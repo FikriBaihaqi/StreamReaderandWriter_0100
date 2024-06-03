79caada77f4f29a94cb80e2e0bb8502ca635d572
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-06-03 15:09:57 +0700

    membuat output awal program sebagai penanda 1

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -3,4 +3,12 @@
 //untuk obyek exception yang akan digunakan
 #include <array>
 //untuk obyek array yang akan kita gunakan
+using namespace std;
 
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:...
+
+
+
+}
