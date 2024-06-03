52a7e1380a444cdbf243eb1e4d5a13419c821fb1
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-06-03 15:02:35 +0700

    sekarang menambahkan catch yang ada di exception

--- manualException.cpp
+++ manualException.cpp
@@ -8,4 +8,10 @@ int main()
 		//throw 0.5; //melemparkan sebuah integer maka
 		cout << "Pengecualian tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+
+	return 0;
 }
